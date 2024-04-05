57c3e856bd5f960a9ebd82989bdb7e89b0babfb3
Bubblegubm <126077486+Bubblegubm@users.noreply.github.com>
2024-04-05 23:29:58 +0700

    Update project

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,12 +1,25 @@
-print("Hello world")
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
-#декоратор для вывода страницы по умолчанию
+# декоратор для вывода страницы по умолчанию
 @app.route("/")
 def hello():
     return " <html><head></head> <body> Hello World! </body></html>"
+
+
+# Новая функция сайта
+@app.route("/data_to")
+def data_to():
+    # Создаем переменные с данными для передачи в шаблон
+    some_pars = {'user': 'Ivan', 'color': 'red'}
+    some_str = 'Hello my dear friends!'
+    some_value = 10
+    # передаем данные в шаблон и вызываем его
+    return render_template('simple.html', some_str=some_str,
+                           some_value=some_value, some_pars=some_pars)
 
 
 if __name__ == "__main__":
